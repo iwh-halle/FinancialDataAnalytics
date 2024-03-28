590090427cd4a893d7015df8ffb25f9924ba2828
woebbeking <woebbeking@axiomatec.com>
2024-03-28 15:47:32 +0100

    2024

--- casestudy/data.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 30 17:29:32 2023
-
-@author: fabian.woebbeking@iwh-halle.de
-"""
-
--- casestudy/trading.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 30 17:30:02 2023
-
-@author: fabian.woebbeking@iwh-halle.de
-"""
